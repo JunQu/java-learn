07e3de15616305164081bfa0965e61379a6abf80
JunQu <junjv@outlook.com>
2023-11-15 23:31:08 +0800

    init project

---
+++ src/test/java/com/qms/lean/QmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qms.lean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
